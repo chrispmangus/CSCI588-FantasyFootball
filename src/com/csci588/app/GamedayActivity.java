eb63649ccd7ce04a3b1aaefae69e887d0f2b8ce5
chrispmangus <mangus@usc.edu>
2011-11-14 14:57:31 -0800

    More UI additions

--- src/com/csci588/app/GamedayActivity.java
+++ src/com/csci588/app/GamedayActivity.java
@@ -17,6 +17,24 @@ public class GamedayActivity extends Activity {
 		SpannableString content = new SpannableString("TOP PERFORMERS");
 		content.setSpan(new UnderlineSpan(), 0 , content.length(), 0);
 		tV.setText(content);
+		
+		/*tV = (TextView) this.findViewById(R.id.myTeamName);
+		tV.setText("MyTeam");
+		
+		tV = (TextView) this.findViewById(R.id.myTeamScore);
+		tV.setText("103.4");
+		
+		tV = (TextView) this.findViewById(R.id.myTeamTime);
+		tV.setText("10 min");
+		
+		tV = (TextView) this.findViewById(R.id.vsTeamName);
+		tV.setText("Opponent Team");
+		
+		tV = (TextView) this.findViewById(R.id.vsTeamScore);
+		tV.setText("92.1");
+		
+		tV = (TextView) this.findViewById(R.id.vsTeamTime);
+		tV.setText("60 min");*/
 	
 	}
 
