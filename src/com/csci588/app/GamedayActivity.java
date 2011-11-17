b3997a2b2962cf1fa08c7cef838bbfea930929a9
chrispmangus <mangus@usc.edu>
2011-11-16 21:34:21 -0800

    gameday finished

--- src/com/csci588/app/GamedayActivity.java
+++ src/com/csci588/app/GamedayActivity.java
@@ -1,13 +1,20 @@
 package com.csci588.app;
 
+import java.io.IOException;
+
 import android.app.Activity;
 import android.content.Context;
-import android.content.Intent;
+import android.database.Cursor;
+import android.database.SQLException;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.text.SpannableString;
 import android.text.style.UnderlineSpan;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+import android.widget.ScrollView;
 import android.widget.TabHost;
 import android.widget.TabHost.TabContentFactory;
 import android.widget.TabHost.TabSpec;
@@ -20,6 +27,8 @@ public class GamedayActivity extends Activity {
 	public void onCreate(Bundle savedInstanceState){
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.gameday);
+		
+		
 		
 		/* Necessary for underlining TOP PERFORMER textview */
 		TextView tV = (TextView) this.findViewById(R.id.topPerformers);
@@ -61,8 +70,78 @@ public class GamedayActivity extends Activity {
 		tV.setText(new SpannableString("20.1"));
 		tV = (TextView) this.findViewById(R.id.vsTopTime);
 		tV.setText(new SpannableString("20 min"));
+		
+		tabs = (TabHost)this.findViewById(R.id.stat_tabhost);
+		tabs.setup();
+
+		
+		
+		dbHelp = new DatabaseHelper(this);
+		try {
+			 
+        	dbHelp.createDataBase();
+ 
+	 	} catch (IOException ioe) {
+	 
+	 		throw new Error("Unable to create database");
+	 
+	 	}
+	 
+	 	try {
+	 
+	 		dbHelp.openDataBase();
+	 
+	 	}catch(SQLException sqle){
+	 
+	 		throw sqle;
+	 
+	 	}
+
+	 	TopPerformerList tpl = new TopPerformerList(this);
+	 	String query = "SELECT NFL_PLAYERS.fName, NFL_PLAYERS.lName FROM NFL_PLAYERS";
+	 	setupTab(tpl.createPerformerList(query, dbHelp), "ALL");
+	 	query = "SELECT NFL_PLAYERS.fName, NFL_PLAYERS.lName FROM NFL_PLAYERS where position_id = 0";
+		setupTab(tpl.createPerformerList(query, dbHelp), "QB");
+		query = "SELECT NFL_PLAYERS.fName, NFL_PLAYERS.lName FROM NFL_PLAYERS where position_id = 2";
+		setupTab(tpl.createPerformerList(query, dbHelp), "RB");
+		query = "SELECT NFL_PLAYERS.fName, NFL_PLAYERS.lName FROM NFL_PLAYERS where position_id = 1";
+		setupTab(tpl.createPerformerList(query, dbHelp), "WR");
+		query = "SELECT NFL_PLAYERS.fName, NFL_PLAYERS.lName FROM NFL_PLAYERS where position_id = 3";
+		setupTab(tpl.createPerformerList(query, dbHelp), "TE");
+		query = "SELECT NFL_PLAYERS.fName, NFL_PLAYERS.lName FROM NFL_PLAYERS where position_id = 4";
+		setupTab(tpl.createPerformerList(query, dbHelp), "K");
+		
+		final ScrollView sv = (ScrollView) this.findViewById(R.id.scrollViewGameDay);
+		sv.post(new Runnable() {            
+		    public void run() {
+		           sv.fullScroll(View.FOCUS_UP);              
+		    }
+		});
+	}
 	
+	private void setupTab(final View view, final String tag){
+		View tabview = createTabView(tabs.getContext(), tag);
+		TabSpec setContent = tabs.newTabSpec(tag).setIndicator(tabview).setContent(new TabContentFactory(){
+			public View createTabContent(String tag){ return view;}
+		});
+		tabs.addTab(setContent);
+	}
+	
+	private static View createTabView(final Context context, final String text){
+		View view = LayoutInflater.from(context).inflate(R.layout.tab_layout, null);
+		TextView tv = (TextView) view.findViewById(R.id.tabText);
+		tv.setText(text);
+		return view;
+	}
+	
+	@Override
+	protected void onDestroy() {
+		super.onDestroy();
+		if (dbHelp != null) {
+			dbHelp.close();
+		}
 	}
 
-
+	private DatabaseHelper dbHelp;
+	private TabHost tabs;
 }
---
+++ src/com/csci588/app/TopPerformerList.java
@@ -0,0 +1,47 @@
+package com.csci588.app;
+
+import android.content.Context;
+import android.database.Cursor;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+public class TopPerformerList {
+
+	private Context context;
+	
+	TopPerformerList(Context context){
+		this.context = context;
+	}
+	
+	public LinearLayout createPerformerList(String query, DatabaseHelper db){
+		LinearLayout ll = new LinearLayout(context);
+		ll.setOrientation(LinearLayout.VERTICAL);
+		Cursor dataCursor = db.getQuery(query);
+		if(dataCursor.moveToFirst()){
+			do{
+				ll.addView(itemCreator(dataCursor));
+			}
+			while(dataCursor.moveToNext());
+		}
+		dataCursor.close();
+		return ll;		
+	}
+	
+	
+	private View itemCreator(Cursor cursor){
+		LayoutInflater lf = (LayoutInflater)context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+		View view = lf.inflate(R.layout.top_performer, null);
+		TextView text = (TextView) view.findViewById(R.id.playerName);
+		text.setText(cursor.getString(0) + " " + cursor.getString(1));
+	 	text = (TextView) view.findViewById(R.id.teamName);
+	 	text.setText(cursor.getString(0) + " " + cursor.getString(1));
+	 	text = (TextView) view.findViewById(R.id.realPoints);
+	 	text.setText("32.2");
+	 	text = (TextView) view.findViewById(R.id.projPoints);
+	 	text.setText("22.1");
+	 	
+	 	return view;
+	}
+}
