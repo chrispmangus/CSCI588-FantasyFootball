a28229106c7750c6406d752ab238cd9eabfa7598
may ang <mayday815@gmail.com>
2011-11-20 23:32:40 -0800

    league standings now with data from database

--- src/com/csci588/app/LeagueActivity.java
+++ src/com/csci588/app/LeagueActivity.java
@@ -18,10 +18,6 @@ import android.widget.TabHost;
 import android.widget.TabHost.TabContentFactory;
 import android.widget.TabHost.TabSpec;
 import android.widget.TextView;
-import android.view.ViewGroup;
-
-
-
 
 public class LeagueActivity extends Activity {
 	
@@ -30,99 +26,15 @@ public class LeagueActivity extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.league_standings);
 		
-		//DatabaseHelper dbHelp = GamedayActivity.getDbHelp();
-		
-		// Temporary Dummy DATA
+		// Populate league standings
+		DatabaseHelper dbHelp = GamedayActivity.getDbHelp();
+		LeagueTeamList ltl = new LeagueTeamList(this, dbHelp);
+		// get user name, wins, losses, ties
+		String query = "select username, wins, losses, ties, managers._id from managers,records where managers._id = records._id order by wins DESC";
 		ViewGroup parent = (ViewGroup) findViewById(R.id.standings);
-		//LayoutInflater lf = (LayoutInflater) this.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-		View v = LayoutInflater.from(getBaseContext()).inflate(R.layout.league_team, null);
-		TextView tv = (TextView) v.findViewById(R.id.teamRank);
-		tv.setText("1");
-		tv = (TextView) v.findViewById(R.id.leagueTeamName);
-		tv.setText("MyTeam");
-		tv = (TextView) v.findViewById(R.id.teamRecord);
-		tv.setText("9-1-0");
-		tv = (TextView) v.findViewById(R.id.teamPoints);
-		tv.setText("998.98");
-		parent.addView(v);
+		ltl.createTeamList(parent, query);
 		
-		v = LayoutInflater.from(getBaseContext()).inflate(R.layout.league_team, null);
-		tv = (TextView) v.findViewById(R.id.teamRank);
-		tv.setText("2");
-		tv = (TextView) v.findViewById(R.id.leagueTeamName);
-		tv.setText("TeamA");
-		tv = (TextView) v.findViewById(R.id.teamRecord);
-		tv.setText("7-3-0");
-		tv = (TextView) v.findViewById(R.id.teamPoints);
-		tv.setText("944.12");
-		parent.addView(v);
 		
-		v = LayoutInflater.from(getBaseContext()).inflate(R.layout.league_team, null);
-		tv = (TextView) v.findViewById(R.id.teamRank);
-		tv.setText("3");
-		tv = (TextView) v.findViewById(R.id.leagueTeamName);
-		tv.setText("TeamB");
-		tv = (TextView) v.findViewById(R.id.teamRecord);
-		tv.setText("6-4-0");
-		tv = (TextView) v.findViewById(R.id.teamPoints);
-		tv.setText("925.12");
-		parent.addView(v);
-		
-		v = LayoutInflater.from(getBaseContext()).inflate(R.layout.league_team, null);
-		tv = (TextView) v.findViewById(R.id.teamRank);
-		tv.setText("4");
-		tv = (TextView) v.findViewById(R.id.leagueTeamName);
-		tv.setText("TeamC");
-		tv = (TextView) v.findViewById(R.id.teamRecord);
-		tv.setText("6-4-0");
-		tv = (TextView) v.findViewById(R.id.teamPoints);
-		tv.setText("844.12");
-		parent.addView(v);
-		
-		v = LayoutInflater.from(getBaseContext()).inflate(R.layout.league_team, null);
-		tv = (TextView) v.findViewById(R.id.teamRank);
-		tv.setText("5");
-		tv = (TextView) v.findViewById(R.id.leagueTeamName);
-		tv.setText("TeamD");
-		tv = (TextView) v.findViewById(R.id.teamRecord);
-		tv.setText("5-5-0");
-		tv = (TextView) v.findViewById(R.id.teamPoints);
-		tv.setText("978.85");
-		parent.addView(v);
-		
-		v = LayoutInflater.from(getBaseContext()).inflate(R.layout.league_team, null);
-		tv = (TextView) v.findViewById(R.id.teamRank);
-		tv.setText("6");
-		tv = (TextView) v.findViewById(R.id.leagueTeamName);
-		tv.setText("TeamE");
-		tv = (TextView) v.findViewById(R.id.teamRecord);
-		tv.setText("5-5-0");
-		tv = (TextView) v.findViewById(R.id.teamPoints);
-		tv.setText("926.26");
-		parent.addView(v);
-		
-		v = LayoutInflater.from(getBaseContext()).inflate(R.layout.league_team, null);
-		tv = (TextView) v.findViewById(R.id.teamRank);
-		tv.setText("7");
-		tv = (TextView) v.findViewById(R.id.leagueTeamName);
-		tv.setText("Teamf");
-		tv = (TextView) v.findViewById(R.id.teamRecord);
-		tv.setText("3-7-0");
-		tv = (TextView) v.findViewById(R.id.teamPoints);
-		tv.setText("785.67");
-		parent.addView(v);
-		
-		v = LayoutInflater.from(getBaseContext()).inflate(R.layout.league_team,null);
-		tv = (TextView) v.findViewById(R.id.teamRank);
-		tv.setText("8");
-		tv = (TextView) v.findViewById(R.id.leagueTeamName);
-		tv.setText("TeamG");
-		tv = (TextView) v.findViewById(R.id.teamRecord);
-		tv.setText("1-9-0");
-		tv = (TextView) v.findViewById(R.id.teamPoints);
-		tv.setText("754.12");
-		parent.addView(v);
-
 		// scrooooollllll
 		final ScrollView sv = (ScrollView) this.findViewById(R.id.standingsScroll);
 		sv.post(new Runnable() {            
--- src/com/csci588/app/LeagueTeamList.java
+++ src/com/csci588/app/LeagueTeamList.java
@@ -1,28 +1,33 @@
 package com.csci588.app;
 
+import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.database.Cursor;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
 public class LeagueTeamList {
 	
 	private Context context;
+	private DatabaseHelper db;
 	
-	LeagueTeamList(Context context){
+	LeagueTeamList(Context context, DatabaseHelper db){
 		this.context = context;
+		this.db = db;
 	}
-	
-	public LinearLayout createTeamList(String query, DatabaseHelper db){
-		LinearLayout ll = new LinearLayout(context);
-		ll.setOrientation(LinearLayout.VERTICAL);
+	 	
+	// Fill list of teams
+	public ViewGroup createTeamList(ViewGroup ll, String query){
 		Cursor dataCursor = db.getQuery(query);
 		if(dataCursor.moveToFirst()){
-			do{
-				ll.addView(itemCreator(dataCursor));
+			int i = 1;
+			do {
+				ll.addView(itemCreator(Integer.toString(i), dataCursor));
+				++i;
 			}
 			while(dataCursor.moveToNext());
 		}
@@ -30,24 +35,35 @@ public class LeagueTeamList {
 		return ll;		
 	}
 	
-	private View itemCreator(Cursor cursor){
+	// Fill team entry
+	private View itemCreator(String rank, Cursor cursor){
 		LayoutInflater lf = (LayoutInflater)context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
 		View view = lf.inflate(R.layout.league_team, null);
 		TextView text = (TextView) view.findViewById(R.id.teamRank);
-		text.setText(cursor.getString(0) + " " + cursor.getString(1));
+		text.setText(rank);
 	 	text = (TextView) view.findViewById(R.id.leagueTeamName);
-	 	text.setText(cursor.getString(0) + " " + cursor.getString(1));
+	 	text.setText(cursor.getString(0));
 	 	text = (TextView) view.findViewById(R.id.teamRecord);
-	 	text.setText(cursor.getString(0) + " " + cursor.getString(1));
+	 	text.setText(cursor.getString(1) + "-" + cursor.getString(2) + "-" + cursor.getString(3));
+	 	final String p_id = cursor.getString(4);
+	 	// TODO: calculate team points
 	 	text = (TextView) view.findViewById(R.id.teamPoints);
-	 	text.setText(cursor.getString(0) + " " + cursor.getString(1));
-	 	final String p_id = cursor.getString(2);
+	 	String totalPoints =  "select sum(real_stats) from nfl_fantasy_stats, rosters " +
+				"where manager_id = "+ cursor.getString(4) + " and rosters.week = 9 and " +
+				"( rosters.qb= _id or rosters.rb1 = _id or rosters.rb2 = _id " +
+				"or rosters.wr1 = _id or rosters.wr2 = _id or rosters.te = _id or rosters.k = _id)";
+	 	Cursor ptCursor = db.getQuery(totalPoints);
+	 	ptCursor.moveToFirst();
+	 	text.setText(Float.toString(ptCursor.getFloat(0)));
+	 	
+	 	// TODO: Get teamId from table using getString(0)
+	 	
 	 	view.setOnClickListener(new View.OnClickListener() {
 			
 			public void onClick(View v) {
 				
-				Intent myIntent = new Intent(v.getContext(), PlayerActivity.class); // need new activity for leagues
-				myIntent.putExtra("team_id", p_id);
+				Intent myIntent = new Intent(v.getContext(), LineupActivity.class); // need new activity for leagues
+				myIntent.putExtra("team_id", p_id); // get team_id using maagnerId
 				v.getContext().startActivity(myIntent);
 			}
 		});
